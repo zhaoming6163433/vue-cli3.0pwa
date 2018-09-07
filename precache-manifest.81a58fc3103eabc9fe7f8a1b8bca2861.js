39afcf35c63f0d8e64867812f80bb00ff84d1ee0
zhaoming <zhaoming6163433@163.com>
2018-09-07 09:40:44 +0800

    tij

---
+++ precache-manifest.81a58fc3103eabc9fe7f8a1b8bca2861.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pwatest/robots.txt"
+  },
+  {
+    "revision": "5c9094383aabd244dfd2",
+    "url": "/pwatest/js/chunk-vendors.8e99d07d.js"
+  },
+  {
+    "revision": "15dc9cf0ef02ac268526",
+    "url": "/pwatest/js/app.c655151d.js"
+  },
+  {
+    "revision": "8092fcb64e07d5daf867",
+    "url": "/pwatest/js/about.54a0da91.js"
+  },
+  {
+    "revision": "69f5bb0a33907b2615fdca39fc20b3ba",
+    "url": "/pwatest/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/pwatest/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "15dc9cf0ef02ac268526",
+    "url": "/pwatest/css/app.a48bfb5d.css"
+  }
+];
---
+++ registerServiceWorker.js
@@ -0,0 +1,29 @@
+importScripts("/pwatest/precache-manifest.81a58fc3103eabc9fe7f8a1b8bca2861.js", "https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
+
+/* eslint-disable no-console */
+
+import { register } from 'register-service-worker'
+
+if (process.env.NODE_ENV === 'production') {
+  register(`${process.env.BASE_URL}service-worker.js`, {
+    ready () {
+      console.log(
+        'App is being served from cache by a service worker.\n' +
+        'For more details, visit https://goo.gl/AFskqB'
+      )
+    },
+    cached () {
+      console.log('Content has been cached for offline use.')
+    },
+    updated () {
+      console.log('New content is available; please refresh.')
+    },
+    offline () {
+      console.log('No internet connection found. App is running in offline mode.')
+    },
+    error (error) {
+      console.error('Error during service worker registration:', error)
+    }
+  })
+}
+
